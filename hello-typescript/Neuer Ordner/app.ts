274bdb8a3c4c623759cae6f8c2c4bb23c8ccbe52
JennisGrief <48830880+JennisGrief@users.noreply.github.com>
2019-12-19 12:02:22 +0100

    stuff

--- hello-typescript/Neuer Ordner/app.ts
+++ hello-typescript/Neuer Ordner/app.ts
@@ -1,2 +1,21 @@
-let canvas: HTMLCanvasElement = document.querySelector("canvas");
-let crc2: CanvasRenderingContext2D = canvas.getContext("2d");
+var canvas = document.querySelector("canvas");
+var crc2 = canvas.getContext("2d");
+
+crc2.fillStyle = "#FF0000"
+crc2.fillRect(0, 0, crc2.canvas.width, crc2.canvas.height);
+
+crc2.beginPath();
+crc2.arc(100, 120, 40, 10, 1.9 * Math.PI);
+crc2.stroke();
+
+crc2.beginPath();
+crc2.ellipse(80, 50, 20, 20, 50, 50, 100,false);
+crc2.stroke();
+
+crc2.beginPath();
+crc2.ellipse(130, 50, 20, 20, 50, 50, 100,false);
+crc2.stroke();
+
+crc2.beginPath();
+crc2.arc(110, 60, 60, 10, 3.1 * Math.PI);
+crc2.stroke();
